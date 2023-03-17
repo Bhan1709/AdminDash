aaadd6cd371ffe2346793c977ae95fddab21ff1a
Bhan Singh <bhans1709@gmail.com>
2023-03-17 20:50:57 +0530

    Added Backend and Frntend for Customers Page

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,22 +1,24 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-    reducerPath: "adminApi",
-    tagTypes: ["User", "Products"],
-    endpoints: (builder) => ({
-        getUser: builder.query({
-            query: (id) => `general/user/${id}`,
-            providesTags: ["User"]
-        }),
-        getProducts: builder.query({
-            query: () => `client/products`,
-            providesTags: ["Products"]
-        })
-    })
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: ["User", "Products", "Customers"],
+  endpoints: (builder) => ({
+    getUser: builder.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: builder.query({
+      query: () => `client/products`,
+      providesTags: ["Products"],
+    }),
+    getCustomers: builder.query({
+      query: () => `client/customers`,
+      providesTags: ["Customers"],
+    }),
+  }),
 });
 
-export const {
-    useGetUserQuery,
-    useGetProductsQuery
-} = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } =
+  api;
